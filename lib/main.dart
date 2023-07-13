5ab80aa7897a9a79827e61b3476e43a6f3629c61
Jeet Rabadiya <jeetrabadiya876@gmail.com>
2023-07-13 19:04:50 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:demo_app/screen/plugin_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const PluginScreen(),
+      home: const MyApp(),
     );
   }
 }
--- lib/screen/plugin_screen.dart
+++ lib/screen/plugin_screen.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:imageview360/imageview360.dart';
 
 class PluginScreen extends StatefulWidget {
   const PluginScreen({Key? key}) : super(key: key);
@@ -13,39 +12,30 @@ class _PluginScreenState extends State<PluginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: ListView(
+        physics: AlwaysScrollableScrollPhysics(),
         children: [
-          CarouselSlider(
-            options: CarouselOptions(height: 400.0),
-            items: [1, 2, 3, 4, 5].map((i) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      decoration: BoxDecoration(color: Colors.amber),
-                      child: Text(
-                        'text $i',
-                        style: TextStyle(fontSize: 16.0),
-                      ));
-                },
-              );
-            }).toList(),
+          Column(
+            children: [
+              CarouselSlider(
+                options: CarouselOptions(height: 400.0),
+                items: [1, 2, 3, 4, 5].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(color: Colors.amber),
+                          child: Text(
+                            'text $i',
+                            style: TextStyle(fontSize: 16.0),
+                          ));
+                    },
+                  );
+                }).toList(),
+              ),
+            ],
           ),
-          ImageView360(
-            key: UniqueKey(),
-            imageList: imageList,
-            autoRotate: true, //Optional
-            rotationCount: 2, //Optional
-            rotationDirection: RotationDirection.anticlockwise, //Optional
-            frameChangeDuration: Duration(milliseconds: 50), //Optional
-            swipeSensitivity: 2, //Optional
-            allowSwipeToRotate: true, //Optional
-            onImageIndexChanged: (currentImageIndex) {
-              //Optional
-              print("currentImageIndex: $currentImageIndex");
-            },
-          )
         ],
       ),
     );
